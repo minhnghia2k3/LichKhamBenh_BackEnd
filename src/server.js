3988fbf14866decb3410a82ddcb818ad0930552e
LE MINH NGHIA <BrozennVN@gmail.com>
2023-01-11 16:48:19 +0700

    update #21

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser"; // take params from users
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web.js";
+import connectDB from "./config/connectDB";
 require('dotenv').config(); // run process.env [line19]
 // /user?id=7 - user body-parsers to get params from id#7
 
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 let port = process.env.PORT || 6969;
 // port equal undefined => port = 6969
