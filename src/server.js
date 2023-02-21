d0a492301b94f41c48cbb0dbdf9df0dd627bd719
LE MINH NGHIA <BrozennVN@gmail.com>
2023-02-21 22:48:46 +0700

    Update #44

--- src/server.js
+++ src/server.js
@@ -3,19 +3,32 @@ import bodyParser from "body-parser"; // take params from users
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web.js";
 import connectDB from "./config/connectDB";
-import cors from 'cors';
+// import cors from 'cors';
 
 require('dotenv').config(); // run process.env [line19]
 // /user?id=7 - user body-parsers to get params from id#7
 
 
 let app = express(); // instance off app express
-const corsOptions = {
-    origin: process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 //config app
